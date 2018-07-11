8b3c4677e93141af79236b628da8d10f4db01ab3
Alex Strand <ajstrand8@gmail.com>
2018-07-10 23:08:16 -0400

    working on fixing sending notifications functionality and ui

--- app/src/main/java/com/example/ajstrand/itf_patterns/CreateNotificationDetails.java
+++ app/src/main/java/com/example/ajstrand/itf_patterns/CreateNotificationDetails.java
@@ -1,18 +1,9 @@
 package com.example.ajstrand.itf_patterns;
 
-import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class CreateNotificationDetails extends AppCompatActivity {
 
@@ -20,7 +11,7 @@ public class CreateNotificationDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_notification_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         if (savedInstanceState == null) {
--- app/src/main/java/com/example/ajstrand/itf_patterns/NotificationPublisher.java
+++ app/src/main/java/com/example/ajstrand/itf_patterns/NotificationPublisher.java
@@ -5,6 +5,7 @@ package com.example.ajstrand.itf_patterns;
  */
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -15,13 +16,23 @@ public class NotificationPublisher extends BroadcastReceiver {
     public static String NOTIFICATION_ID = "notification-id";
     public static String NOTIFICATION = "notification";
 
+    public static final String NOTIFICATION_CHANNEL_ID = "4655";
+
+    //Notification Channel
+    CharSequence channelName = "foo";
+    int importance = NotificationManager.IMPORTANCE_LOW;
+    NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, importance);
+
+
     public void onReceive(Context context, Intent intent) {
+
 
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         Notification notification = intent.getParcelableExtra(NOTIFICATION);
         int id = intent.getIntExtra(NOTIFICATION_ID, 0);
         notificationManager.notify(id, notification);
+        notificationManager.createNotificationChannel(notificationChannel);
 
     }
 }
